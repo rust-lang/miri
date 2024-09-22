155e965deb501c37a61ff12d3029d976917f46d3
Yoh Deadfall <yoh.deadfall@hotmail.com>
2024-09-22 23:03:28 +0300

    Reworked tests to call prctl directly

--- tests/pass-dep/libc/prctl.rs
+++ tests/pass-dep/libc/prctl.rs
@@ -1,56 +1,46 @@
-//@ignore-target: windows # No prctl on Windows
-//@ignore-target: apple # No prctl on macOS
+//@only-target: android # prctl is currently supported for Android only
 use std::ffi::CStr;
 use std::ffi::CString;
-use std::thread;
 
 fn main() {
-    let long_name = std::iter::once("test_named_thread_truncation")
-        .chain(std::iter::repeat(" yada").take(100))
-        .collect::<String>();
+    let long_name = CString::new(
+        std::iter::once("test_named_thread_truncation")
+            .chain(std::iter::repeat(" yada").take(100))
+            .collect::<String>(),
+    )
+    .unwrap();
 
-    fn set_thread_name(name: &CStr) -> i32 {
-        cfg_if::cfg_if! {
-            if #[cfg(any(target_os = "linux", target_os = "illumos", target_os = "solaris", target_os = "freebsd"))] {
-                unsafe { libc::pthread_setname_np(libc::pthread_self(), name.as_ptr().cast()) }
-            } else {
-                compile_error!("set_thread_name not supported for this OS")
-            }
+    // prctl supports thread names up to 16 characters includding nul.
+    // And since the input string is longer it should fail.
+    assert_ne!(set_thread_name(&long_name), 0);
+
+    let cstr = CString::new(&long_name.as_bytes()[..15]).unwrap();
+    assert_eq!(set_thread_name(&cstr), 0);
+
+    let mut buf = vec![0u8; 16];
+    assert_eq!(get_thread_name(&mut buf), 0);
+}
+
+fn set_thread_name(name: &CStr) -> i32 {
+    cfg_if::cfg_if! {
+        if #[cfg(any(target_os = "android"))] {
+            unsafe { libc::prctl(libs::PR_SET_NAME, name.as_ptr().cast()) }
+        } else {
+            compile_error!("set_thread_name not supported for this OS")
         }
     }
+}
 
-    fn get_thread_name(name: &mut [u8]) -> i32 {
-        cfg_if::cfg_if! {
-            if #[cfg(any(
-                target_os = "linux",
-                target_os = "illumos",
-                target_os = "solaris",
-                target_os = "freebsd",
-            ))] {
-                unsafe {
-                    libc::pthread_getname_np(libc::pthread_self(), name.as_mut_ptr().cast(), name.len())
-                }
-            } else {
-                compile_error!("get_thread_name not supported for this OS")
+fn get_thread_name(name: &mut [u8]) -> i32 {
+    cfg_if::cfg_if! {
+        if #[cfg(any(
+            target_os = "android",
+        ))] {
+            unsafe {
+                libc::prctl(libc::PR_GET_NAME, name.as_mut_ptr().cast())
             }
+        } else {
+            compile_error!("get_thread_name not supported for this OS")
         }
     }
-
-    let result = thread::Builder::new().name(long_name.clone()).spawn(move || {
-        // Rust remembers the full thread name itself.
-        assert_eq!(thread::current().name(), Some(long_name.as_str()));
-
-        // But the system is limited -- make sure we successfully set a truncation.
-        let mut buf = vec![0u8; long_name.len() + 1];
-        assert_eq!(get_thread_name(&mut buf), 0);
-        let cstr = CStr::from_bytes_until_nul(&buf).unwrap();
-        assert!(cstr.to_bytes().len() >= 15, "name is too short: len={}", cstr.to_bytes().len()); // POSIX seems to promise at least 15 chars
-        assert!(long_name.as_bytes().starts_with(cstr.to_bytes()));
-
-        // Also test directly calling pthread_setname to check its return value.
-        assert_eq!(set_thread_name(&cstr), 0);
-        // But with a too long name it should fail.
-        assert_ne!(set_thread_name(&CString::new(long_name).unwrap()), 0);
-    });
-    result.unwrap().join().unwrap();
 }
