989c19b98e838eca944a004a1a7fb7d5df292e38
Iris Shi <0.0@owo.li>
2025-02-27 11:41:25 +0800

    fix perm width and things

--- src/shims/unix/fs.rs
+++ src/shims/unix/fs.rs
@@ -1668,52 +1668,50 @@ pub trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
     fn chmod(&mut self, path_op: &OpTy<'tcx>, perm_op: &OpTy<'tcx>) -> InterpResult<'tcx, Scalar> {
         let this = self.eval_context_mut();
 
-        // Permissions::from_mode is Unix-specific.
-        this.assert_target_os_is_unix("chmod");
+        let pathname = this.read_path_from_c_str(this.read_pointer(path_op)?)?;
+        let perm = this.read_scalar(perm_op)?.to_uint(this.libc_ty_layout("mode_t").size)?;
 
+        // Reject if isolation is enabled.
+        if let IsolatedOp::Reject(reject_with) = this.machine.isolated_op {
+            this.reject_in_isolation("`chmod`", reject_with)?;
+            return this.set_last_error_and_return_i32(LibcError("EACCES"));
+        }
+
+        // Permissions::from_mode is Unix-specific.
         #[cfg(unix)]
         {
             use std::os::unix::fs::PermissionsExt;
 
-            let pathname = this.read_path_from_c_str(this.read_pointer(path_op)?)?;
-            let perm = this.read_scalar(perm_op)?.to_u32()?;
-
-            // Reject if isolation is enabled.
-            if let IsolatedOp::Reject(reject_with) = this.machine.isolated_op {
-                this.reject_in_isolation("`chmod`", reject_with)?;
-                return this.set_last_error_and_return_i32(LibcError("EACCES"));
-            }
-
-            let result = std::fs::set_permissions(pathname, Permissions::from_mode(perm));
+            let result = std::fs::set_permissions(
+                pathname,
+                Permissions::from_mode(perm.try_into().unwrap()),
+            );
             let result = this.try_unwrap_io_result(result.map(|_| 0i32))?;
 
             interp_ok(Scalar::from_i32(result))
         }
         #[cfg(not(unix))]
         {
-            unreachable!()
+            throw_unsup_format!("`chmod` is not supported on this platform")
         }
     }
 
     fn fchmod(&mut self, fd_op: &OpTy<'tcx>, perm_op: &OpTy<'tcx>) -> InterpResult<'tcx, Scalar> {
         let this = self.eval_context_mut();
 
-        // `Permissions::from_mode` is Unix-specific.
-        this.assert_target_os_is_unix("fchmod");
+        let fd = this.read_scalar(fd_op)?.to_i32()?;
+        let perm = this.read_scalar(perm_op)?.to_uint(this.libc_ty_layout("mode_t").size)?;
 
+        // Reject if isolation is enabled.
+        if let IsolatedOp::Reject(reject_with) = this.machine.isolated_op {
+            this.reject_in_isolation("`fchmod`", reject_with)?;
+            // Set error code as "EBADF" (bad fd)
+            return this.set_last_error_and_return_i32(LibcError("EBADF"));
+        }
+        // `Permissions::from_mode` is Unix-specific.
         #[cfg(unix)]
         {
             use std::os::unix::fs::PermissionsExt;
-
-            let fd = this.read_scalar(fd_op)?.to_i32()?;
-            let perm = this.read_scalar(perm_op)?.to_u32()?;
-
-            // Reject if isolation is enabled.
-            if let IsolatedOp::Reject(reject_with) = this.machine.isolated_op {
-                this.reject_in_isolation("`fchmod`", reject_with)?;
-                // Set error code as "EBADF" (bad fd)
-                return this.set_last_error_and_return_i32(LibcError("EBADF"));
-            }
 
             let Some(fd) = this.machine.fds.get(fd) else {
                 return this.set_last_error_and_return_i32(LibcError("EBADF"));
@@ -1723,14 +1721,15 @@ pub trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
                 err_unsup_format!("`fchmod` is only supported on file-backed file descriptors")
             })?;
 
-            let result = file.file.set_permissions(Permissions::from_mode(perm));
+            let result =
+                file.file.set_permissions(Permissions::from_mode(perm.try_into().unwrap()));
             let result = this.try_unwrap_io_result(result.map(|_| 0i32))?;
 
             interp_ok(Scalar::from_i32(result))
         }
         #[cfg(not(unix))]
         {
-            unreachable!()
+            throw_unsup_format!("`fchmod` is not supported on this platform")
         }
     }
 }
