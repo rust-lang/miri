cff2beb1ef6fa55967cef125b163033bb85606d5
Patrick-6 <pamu99@gmx.ch>
2025-08-29 17:09:44 +0200

    Clean up test and add comments

--- tests/genmc/fail/simple/2w2w_weak.rs
+++ tests/genmc/fail/simple/2w2w_weak.rs
@@ -1,52 +1,60 @@
 //@compile-flags: -Zmiri-genmc -Zmiri-disable-stacked-borrows
-//@revisions: acq_rel relaxed
+//@revisions: seqcst_rel acqrel relaxed
 
-// This test is the equivalent to the `2w2w_seqcst.rs` "pass" test.
-// Here we use weaker atomic memory orderings to test if we can encounter
-// an execution where (X == 1 && Y == 1).
+// This test is the equivalent to the litmus "pass" test `2w2w_3sc_rel1`.
+// Here we test different atomic orderings that should all allow an execution where (X, Y) == (1, 1) at the end.
+//
+// Miri without GenMC is unable to produce this program execution, even with -Zmiri-many-seeds.
 
 #![no_main]
 
 #[path = "../../../utils/genmc.rs"]
 mod genmc;
 
-use std::ffi::c_void;
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::atomic::AtomicU64;
+use std::sync::atomic::Ordering::{self, *};
 
-use crate::genmc::{join_pthread, spawn_pthread};
+use crate::genmc::{join_pthreads, spawn_pthread_closure};
 
 static X: AtomicU64 = AtomicU64::new(0);
 static Y: AtomicU64 = AtomicU64::new(0);
 
-#[cfg(acq_rel)]
-const LOAD_ORD: Ordering = Ordering::Acquire;
-#[cfg(acq_rel)]
-const STORE_ORD: Ordering = Ordering::Release;
+#[cfg(seqcst_rel)]
+const STORE_ORD_3: Ordering = SeqCst;
+#[cfg(seqcst_rel)]
+const STORE_ORD_1: Ordering = Release;
 
-#[cfg(not(acq_rel))]
-const LOAD_ORD: Ordering = Ordering::Relaxed;
-#[cfg(not(acq_rel))]
-const STORE_ORD: Ordering = Ordering::Relaxed;
+#[cfg(acqrel)]
+const STORE_ORD_3: Ordering = Release;
+#[cfg(acqrel)]
+const STORE_ORD_1: Ordering = Release;
+
+#[cfg(not(any(acqrel, seqcst_rel)))]
+const STORE_ORD_3: Ordering = Relaxed;
+#[cfg(not(any(acqrel, seqcst_rel)))]
+const STORE_ORD_1: Ordering = Relaxed;
 
 #[unsafe(no_mangle)]
 fn miri_start(_argc: isize, _argv: *const *const u8) -> isize {
-    let thread_id = unsafe { spawn_pthread(thread_func, std::ptr::null_mut()) };
+    unsafe {
+        let ids = [
+            spawn_pthread_closure(|| {
+                X.store(1, STORE_ORD_3);
+                Y.store(2, STORE_ORD_3);
+            }),
+            spawn_pthread_closure(|| {
+                Y.store(1, STORE_ORD_1);
+                X.store(2, STORE_ORD_3);
+            }),
+        ];
+        // Join so we can read the final values.
+        join_pthreads(ids);
 
-    X.store(1, STORE_ORD);
-    Y.store(2, STORE_ORD);
+        let result = (X.load(Relaxed), Y.load(Relaxed));
+        if result == (1, 1) {
+            std::hint::unreachable_unchecked(); //~ ERROR: entering unreachable code
+        }
 
-    unsafe { join_pthread(thread_id) };
-
-    let x = X.load(LOAD_ORD);
-    let y = Y.load(LOAD_ORD);
-    if x == 1 && y == 1 {
-        unsafe { std::hint::unreachable_unchecked() }; //~ ERROR: entering unreachable code
+        0
     }
-    0
-}
-
-extern "C" fn thread_func(_value: *mut c_void) -> *mut c_void {
-    Y.store(1, STORE_ORD);
-    X.store(2, STORE_ORD);
-    std::ptr::null_mut()
 }
