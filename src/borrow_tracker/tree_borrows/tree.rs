0d40e7b2fed01fb25c360b7d361819a01bb6756f
Roy Ammerschuber <roy@ammerschuber.de>
2025-10-19 14:40:44 +0200

    fix formatting

--- src/borrow_tracker/tree_borrows/tree.rs
+++ src/borrow_tracker/tree_borrows/tree.rs
@@ -1187,7 +1187,6 @@ impl<'tcx> Tree {
                     let mut entry = wildcard_accesses.entry(id);
                     let wildcard_access = entry.or_insert(Default::default());
 
-
                     let exposed_as = node.exposed_as(Some(perm.permission));
                     let Some(wildcard_relatedness) =
                         wildcard_access.access_relatedness(access_kind, exposed_as)
@@ -1202,8 +1201,11 @@ impl<'tcx> Tree {
                         ))
                         .into();
                     };
-                    if let Some(relatedness)=wildcard_relatedness.to_relatedness()
-                        && matches!(perm.skip_if_known_noop(access_kind, relatedness), ContinueTraversal::SkipSelfAndChildren)
+                    if let Some(relatedness) = wildcard_relatedness.to_relatedness()
+                        && matches!(
+                            perm.skip_if_known_noop(access_kind, relatedness),
+                            ContinueTraversal::SkipSelfAndChildren
+                        )
                     {
                         continue;
                     }
