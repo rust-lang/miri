1e503507ab3298d5f0decfa6d2ee9d3f01b69c52
Roy Ammerschuber <roy@ammerschuber.de>
2025-10-29 13:55:26 +0100

    formatting...

--- tests/fail/tree_borrows/wildcard/gc.rs
+++ tests/fail/tree_borrows/wildcard/gc.rs
@@ -3,18 +3,18 @@
 #[path = "../../../utils/mod.rs"]
 mod utils;
 
-#[allow(unused_variables,unused_assignments)]
+#[allow(unused_variables, unused_assignments)]
 fn main() {
-    let mut x:u32=4;
+    let mut x: u32 = 4;
     let int = {
-        let y=&x;
+        let y = &x;
         y as *const u32 as usize
     };
     // If y wasn't exposed, this would gc it.
     utils::run_provenance_gc();
     // This should disable y.
-    x=5;
-    let wild=int as *const u32;
+    x = 5;
+    let wild = int as *const u32;
 
-    let fail=unsafe{ *wild }; //~ ERROR: /read access through wildcard at .* is forbidden/
+    let fail = unsafe { *wild }; //~ ERROR: /read access through wildcard at .* is forbidden/
 }
