f880dca8fa37f0c1066bc47522e3947c15497ed9
Rune Tynan <runetynan@gmail.com>
2025-12-12 11:53:05 -0800

    Implement FlushFileBuffers

--- src/shims/windows/foreign_items.rs
+++ src/shims/windows/foreign_items.rs
@@ -307,6 +307,11 @@ pub trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
                 let res = this.GetFileInformationByHandle(handle, info)?;
                 this.write_scalar(res, dest)?;
             }
+            "FlushFileBuffers" => {
+                let [handle] = this.check_shim_sig_lenient(abi, sys_conv, link_name, args)?;
+                let res = this.FlushFileBuffers(handle)?;
+                this.write_scalar(res, dest)?;
+            }
             "DeleteFileW" => {
                 let [file_name] = this.check_shim_sig_lenient(abi, sys_conv, link_name, args)?;
                 let res = this.DeleteFileW(file_name)?;
