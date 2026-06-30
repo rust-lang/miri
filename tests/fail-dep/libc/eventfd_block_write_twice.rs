d5325b386412e4cfe08526767d1e15653748a2cd
TheDoctor314 <64731940+TheDoctor314@users.noreply.github.com>
2026-06-30 22:08:37 +0530

    Wrap `libc::read/write` calls in `errno_result`

--- tests/fail-dep/libc/eventfd_block_write_twice.rs
+++ tests/fail-dep/libc/eventfd_block_write_twice.rs
@@ -24,16 +24,17 @@ fn main() {
     let fd = errno_result(unsafe { libc::eventfd(0, flags) }).unwrap();
     // Write u64 - 1, so the all subsequent write will block.
     let sized_8_data: [u8; 8] = (u64::MAX - 1).to_ne_bytes();
-    let res: i64 = unsafe {
-        libc::write(fd, sized_8_data.as_ptr() as *const libc::c_void, 8).try_into().unwrap()
-    };
+    let res =
+        errno_result(unsafe { libc::write(fd, sized_8_data.as_ptr() as *const libc::c_void, 8) })
+            .unwrap();
     assert_eq!(res, 8);
 
     let thread1 = thread::spawn(move || {
         let sized_8_data = (u64::MAX - 1).to_ne_bytes();
-        let res: i64 = unsafe {
-            libc::write(fd, sized_8_data.as_ptr() as *const libc::c_void, 8).try_into().unwrap()
-        };
+        let res = errno_result(unsafe {
+            libc::write(fd, sized_8_data.as_ptr() as *const libc::c_void, 8)
+        })
+        .unwrap();
         // Make sure that write is successful.
         assert_eq!(res, 8);
     });
@@ -41,12 +42,13 @@ fn main() {
     let thread2 = thread::spawn(move || {
         let sized_8_data = (u64::MAX - 1).to_ne_bytes();
         // Write u64::MAX - 1, so that all subsequent writes will block.
-        let res: i64 = unsafe {
+        let res = errno_result(unsafe {
             // This `write` will initially blocked, then get unblocked by thread3, then get blocked again
             // because the `write` in thread1 executes first.
-            libc::write(fd, sized_8_data.as_ptr() as *const libc::c_void, 8).try_into().unwrap()
+            libc::write(fd, sized_8_data.as_ptr() as *const libc::c_void, 8)
             //~^ERROR: deadlocked
-        };
+        })
+        .unwrap();
         // Make sure that write is successful.
         assert_eq!(res, 8);
     });
@@ -54,7 +56,7 @@ fn main() {
     let thread3 = thread::spawn(move || {
         let mut buf: [u8; 8] = [0; 8];
         // This will unblock both `write` in thread1 and thread2.
-        let res: i64 = unsafe { libc::read(fd, buf.as_mut_ptr().cast(), 8).try_into().unwrap() };
+        let res = errno_result(unsafe { libc::read(fd, buf.as_mut_ptr().cast(), 8) }).unwrap();
         assert_eq!(res, 8);
         let counter = u64::from_ne_bytes(buf);
         assert_eq!(counter, (u64::MAX - 1));
